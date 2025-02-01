25f465cebd7b82123f908d028906e103f75a4535
Rohan S. <75220219+r0hb0t@users.noreply.github.com>
2025-02-01 17:15:33 -0500

    Randomize color theme and add image of random guy to index

--- header.js
+++ header.js
@@ -12,3 +12,13 @@ let header = `<div class="slideMenu">
     </div>
 `;
 document.getElementById("header").innerHTML = header;
+
+function onLoad() {
+    var root = document.querySelector(":root");
+    var colors = [["1f008f","ff5100"],["551100","55cc00"],["220066","ee9900"]];
+    
+    var randomColor = Math.round((Math.random() * (colors.length - 1)));
+    
+    root.style.setProperty('--shadow',"#"+colors[randomColor][0]);
+    root.style.setProperty('--light',"#"+colors[randomColor][1]);
+}
