ebf3bd83af749bdda36c43f98c8ed40644fe2f2c
Rohan S. <75220219+r0hb0t@users.noreply.github.com>
2025-02-01 12:29:14 -0500

    Add JavaScript header and fix collapsed menu showing on screen

---
+++ header.js
@@ -0,0 +1,14 @@
+let header = `<div class="slideMenu">
+        <input type="checkbox"><span class="toggleButton"><p>.</p></span></input>
+
+        <div class="slideContent">
+            <a href="index.html">Home</a>
+            <!--<a href="about.html">About</a>-->
+            <a href="projects.html">Projects</a>
+            <a href="resources.html">Resources</a>
+            <a href="index-old.html">[OLD] Home</a>
+            <a href="projects-old.html">[OLD] Projects</a>
+        </div>
+    </div>
+`;
+document.getElementById("header").innerHTML = header;
